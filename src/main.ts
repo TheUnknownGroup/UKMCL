b4ac86e2e51ee38fb76f8c6535a655f4cf461d3e
devonk15 <94760159+devonk15@users.noreply.github.com>
2024-12-20 14:43:48 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 const sign = document.getElementById("y");
 const log = document.getElementById("n");
-
+// @ts-ignore
 sign.addEventListener("click", ()=> {
     location.href = "./src/html/login.html";
 });
-
+// @ts-ignore
 log.addEventListener("click", () => {
     location.href = "./src/html/signup.html";
 })
