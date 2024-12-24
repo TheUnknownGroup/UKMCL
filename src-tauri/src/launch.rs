607062722a5034d13f25b3337c6dd477edf2724f
devonk15 <94760159+devonk15@users.noreply.github.com>
2024-12-24 16:42:54 -0500

    Delete src-tauri directory

--- src-tauri/src/launch.rs
+++
@@ -1,6 +0,0 @@
-use std::process::Command;
-
-pub fn launch_minecraft(java_path: &str, args: Vec<&str>) -> anyhow::Result<()> {
-    Command::new(java_path).args(args).spawn()?.wait()?;
-    Ok(())
-}
