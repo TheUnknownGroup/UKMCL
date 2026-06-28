7711f8b1497b56415acdd5107306a76b989acb31
devonk15 <94760159+devonk15@users.noreply.github.com>
2026-06-28 18:08:50 -0400

    just a test

--- src/main/kotlin/xyz/theunknowngroup/UkmclMain.kt
+++ src/main/kotlin/xyz/theunknowngroup/UkmclMain.kt
@@ -1,7 +1,7 @@
 package xyz.theunknowngroup
 
 import javafx.application.Application
-import xyz.theunknowngroup.app.App;
+import xyz.theunknowngroup.app.App
 
 fun main() {
     Application.launch(App::class.java)
